9b4f58d3add0483ece06ea00eef4732986f50517
tonyguo <tonyguo2010@gmail.com>
2020-07-03 18:59:06 -0400

    2010.3 Punchy

--- src/com/seakie/C2010.java
+++ src/com/seakie/C2010.java
@@ -1,8 +1,12 @@
 package com.seakie;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class C2010 {
+
+	static int numberA = 0;
+	static int numberB = 0;
 
 	public static void q1WhatIs() {
 		Scanner scan = new Scanner(System.in);
@@ -66,6 +70,154 @@ public class C2010 {
 		}
 
 		scan.close();
+	}
+
+	public static void q3Punchy() {
+		Scanner scan = new Scanner(System.in);
+		
+		while (true) {
+			String line = scan.nextLine();
+			String[] segments = line.split(" ");
+			
+			int state = getState(segments);
+			
+			if (state == 7) {
+				break;
+			}
+//			System.out.println(Arrays.toString(segments));
+			switch (state) {
+			case 1:
+				setValue(segments);
+				break;
+			case 2:
+				outputValue(segments);
+				break;
+			case 3:
+				addValue(segments);
+				break;
+			case 4:
+				multiplyValue(segments);
+				break;
+			case 5:
+				subValue(segments);
+				break;
+			case 6:
+				divideValue(segments);
+				break;
+
+			default:
+				break;
+			}
+		}
+		scan.close();
+	}
+
+	private static void divideValue(String[] segments) {
+		int first = 0;
+		int second = 0;
+		
+		if (segments[1].equals("A")) {
+			first = numberA;
+		} else if (segments[1].equals("B")) {
+			first = numberB;
+		}
+		if (segments[2].equals("A")) {
+			second = numberA;
+		} else if (segments[2].equals("B")) {
+			second = numberB;
+		}
+		
+		if (segments[1].equals("A")) {
+			numberA = first / second;
+		} else if (segments[1].equals("B")) {
+			numberB = first / second;
+		}
+	}
+
+	private static void subValue(String[] segments) {
+		int first = 0;
+		int second = 0;
+		
+		if (segments[1].equals("A")) {
+			first = numberA;
+		} else if (segments[1].equals("B")) {
+			first = numberB;
+		}
+		if (segments[2].equals("A")) {
+			second = numberA;
+		} else if (segments[2].equals("B")) {
+			second = numberB;
+		}
+		
+		if (segments[1].equals("A")) {
+			numberA = first - second;
+		} else if (segments[1].equals("B")) {
+			numberB = first - second;
+		}
+	}
+
+	private static void multiplyValue(String[] segments) {
+		int first = 0;
+		int second = 0;
+		
+		if (segments[1].equals("A")) {
+			first = numberA;
+		} else if (segments[1].equals("B")) {
+			first = numberB;
+		}
+		if (segments[2].equals("A")) {
+			second = numberA;
+		} else if (segments[2].equals("B")) {
+			second = numberB;
+		}
+		
+		if (segments[1].equals("A")) {
+			numberA = first * second;
+		} else if (segments[1].equals("B")) {
+			numberB = first * second;
+		}
+	}
+
+	private static void addValue(String[] segments) {
+		int first = 0;
+		int second = 0;
+		
+		if (segments[1].equals("A")) {
+			first = numberA;
+		} else if (segments[1].equals("B")) {
+			first = numberB;
+		}
+		if (segments[2].equals("A")) {
+			second = numberA;
+		} else if (segments[2].equals("B")) {
+			second = numberB;
+		}
+		
+		if (segments[1].equals("A")) {
+			numberA = first + second;
+		} else if (segments[1].equals("B")) {
+			numberB = first + second;
+		}
+	}
+
+	private static void outputValue(String[] segments) {
+		if (segments[1].equals("A")) {
+			System.out.println(numberA);
+		} else if (segments[1].equals("B")) {
+			System.out.println(numberB);
+		}
+	}
+
+	private static void setValue(String[] segments) {
+		if (segments[1].equals("A")) {
+			numberA = Integer.parseInt(segments[2]);
+		} else if (segments[1].equals("B")) {
+			numberB = Integer.parseInt(segments[2]);
+		}
+	}
+
+	private static int getState(String[] segments) {
+		return Integer.parseInt(segments[0]);
 	}
 
 }
--- src/com/seakie/Main.java
+++ src/com/seakie/Main.java
@@ -3,7 +3,9 @@ package com.seakie;
 public class Main {
 
 	public static void main(String[] args) {
-		C2020.q5QuitRoom();
+		C2010.q3Punchy();
+		
+//		ACM.numberTower();
 	}
 
 }
